20286b141e73b18877663b8d104191af84bbefdb
Baochuan Lu <blu@sbuniv.edu>
2017-09-19 20:04:38 +0000

    add starter code

---
+++ EmptyCollectionException.java
@@ -0,0 +1,13 @@
+/**
+ * EmptyCollectionException represents the situation in which a collection
+ * is empty.
+ */
+
+public class EmptyCollectionException extends RuntimeException{
+  /**
+  * Sets up this exception with an appropriate message.
+  */
+  public EmptyCollectionException (String collection){
+    super ("The " + collection + " is empty.");
+  }
+}
